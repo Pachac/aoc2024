7a3c415896732dfff143d7269b2bf3b9dd30861f
Tomas Pachovsky <pachovst@gmail.com>
2024-12-08 15:05:15 +0100

    add part 1 to day2

--- aoc2024/src/bin/day2/day2.rs
+++ aoc2024/src/bin/day2/day2.rs
@@ -49,9 +49,9 @@ fn check_report_safety(report: &Vec<i32>, problem_dampener: bool) -> bool {
 pub fn run(input_path: String) {
     let input = fs::read_to_string(input_path).expect("Unable to read file");
     let reports = parse_input(input);
-    //let valid_reports1 = reports.iter().filter(|x| check_report_safety(x, false)).count();
+    let valid_reports1 = reports.iter().filter(|x| check_report_safety(x, false)).count();
     let valid_reports2 = reports.iter().filter(|x| check_report_safety(x, true)).count();
     
-    //println!("Part 1 result: {:?}", valid_reports1);
+    println!("Part 1 result: {:?}", valid_reports1);
     println!("Part 2 result: {:?}", valid_reports2);
 }
